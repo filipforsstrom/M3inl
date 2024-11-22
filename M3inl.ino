1d84a2a24b50e851ddb5ce889cac815f600d2113
Filip Forsström <83241499+filipforsstrom@users.noreply.github.com>
2024-11-22 18:47:53 +0100

    with rtc

--- M3inl.ino
+++ M3inl.ino
@@ -1,4 +1,5 @@
 #include "InputDebounce.h"
+#include "RTC.h"
 
 #define BUTTON_DEBOUNCE_DELAY 20 // in ms
 
@@ -10,13 +11,20 @@ bool activeSensor = true;
 
 void buttonTest_pressedCallback(uint8_t pinIn)
 {
+	RTCTime currenttime;
+	RTC.getTime(currenttime);
+
 	activeSensor = !activeSensor;
 	if (activeSensor)
 	{
+		Serial1.print(currenttime.toString());
+		Serial1.println(": Started reading sensor");
 		digitalWrite(LED_BUILTIN, LOW);
 	}
 	else
 	{
+		Serial1.print(currenttime.toString());
+		Serial1.println(": Stopped reading sensor");
 		digitalWrite(LED_BUILTIN, HIGH);
 	}
 }
@@ -42,6 +50,29 @@ void setup()
 	buttonA.registerCallbacks(buttonTest_pressedCallback, buttonTest_releasedCallback, buttonTest_pressedDurationCallback, buttonTest_releasedDurationCallback);
 	buttonA.setup(pinButton, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
 
+	RTC.begin();
+
+	// A fallback time object, for setting the time if there is no time to retrieve from the RTC.
+	RTCTime mytime(6, Month::NOVEMBER, 2023, 18, 12, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+
+	// Tries to retrieve time
+	RTCTime savedTime;
+
+	RTC.getTime(savedTime);
+
+	if (!RTC.isRunning())
+	{
+		// this means the RTC is waking up "as new"
+		if (savedTime.getYear() == 2000)
+		{
+			RTC.setTime(mytime);
+		}
+		else
+		{
+			RTC.setTime(savedTime);
+		}
+	}
+
 	// examples
 	// buttonA.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, buttonTest_releasedDurationCallback); // no continuous pressed-on time duration, ...
 	// buttonA.setup(pinButton);
@@ -52,6 +83,9 @@ void setup()
 
 void loop()
 {
+	RTCTime currenttime;
+	RTC.getTime(currenttime);
+
 	unsigned long now = millis();
 
 	buttonA.process(now);
